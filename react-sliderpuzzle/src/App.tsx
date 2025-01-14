5738fa167858df10750408855cf87ce3f540dd53
mindyBarrs <barrs.mindy@gmail.com>
2025-01-14 13:14:06 -0500

    added files, setup for typescript and cleaned up

---
+++ react-sliderpuzzle/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import StartPanel from "feature/startPanel";
+
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">Slider Pizzle</header>
+
+			<StartPanel />
+		</div>
+	);
+}
+
+export default App;
---
+++ react-sliderpuzzle/src/feature/startPanel.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// Your component code here
+
+const StartPanel = () => {
+	return <div>{/* Your component JSX here */}</div>;
+};
+
+export default StartPanel;
